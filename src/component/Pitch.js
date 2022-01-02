5d87ae03ee6855df2ac236cca146655f64f82415
Juan Pablo Luncumilla <jpluncumilla@Juans-MacBook-Pro.local>
2022-01-02 18:04:12 -0500

    Next pitch funcionality that adds pitch into array

--- src/component/Pitch.js
+++ src/component/Pitch.js
@@ -1,50 +1,77 @@
 import React, { useState} from 'react';
 import "./Pitch.css";
+import driveline from "../images/driveline.png"
 
 export const Pitch = () => {
 
-    const [firstClick, setFirstClick] = useState(0);
-    const [secondClick, setSecondClick] = useState(0);
+    const [firstClick, setFirstClick] = useState(null);
+    const [secondClick, setSecondClick] = useState(null);
+   
+    /* Empty array to be filled */
+    const [theArray, setTheArray] = useState([]);
+
+    /* Something to save current states as object and then push into empty array*/
+    const nextPitch = () =>{
+        if(firstClick !== null && secondClick !== null){
+            const updatePitch = [
+                ...theArray,
+                {
+                    coach: firstClick,
+                    pitcher: secondClick
+                }
+            ]
+            setTheArray(updatePitch)
+            reset();
+            console.log(theArray)
+        } else{
+            alert("Please enter a desired Pitch and where Pitch actually landed.")
+        }
+
+    }
+    
 
     const reset = () => {
-        setFirstClick(0);
-        setSecondClick(0);
+        setFirstClick(null);
+        setSecondClick(null);
     }
+
+    const requestedPitch = firstClick === null ? <img src={driveline} alt="Desired Pitch" /> : null;
 
     return(
         <div className='Pitch'>
 
                 <div className='first-row'>
-                        <div value={11} onClick={() => firstClick === 0 ? setFirstClick(11) : secondClick === 0 ? setSecondClick(11): secondClick}>11</div>
+                        <div value={11} onClick={() => firstClick === null ? setFirstClick(11) : secondClick === null ? setSecondClick(11): secondClick}>11</div>
                 </div>
 
                 <div className='second-row'>
-                    <div className='second-row-inner' value={12} onClick={() => firstClick === 0 ? setFirstClick(12) : secondClick === 0 ? setSecondClick(12): secondClick}>12</div>
-
+                    <div className='second-row-inner' value={12} onClick={() => firstClick === null ? setFirstClick(12) : secondClick === null ? setSecondClick(12): secondClick}>12</div>
+                    
                     <div className='middle-group'>
-                        <div className='middle-group-inner' value={7} onClick={() => firstClick === 0 ? setFirstClick(7) : secondClick === 0 ? setSecondClick(7): secondClick}>7</div>
-                        <div className='middle-group-inner' value={8} onClick={() => firstClick === 0 ? setFirstClick(8) : secondClick === 0 ? setSecondClick(8): secondClick}>8</div>
-                        <div className='middle-group-inner' value={9} onClick={() => firstClick === 0 ? setFirstClick(9) : secondClick === 0 ? setSecondClick(9): secondClick}>9</div>
-                        <div className='middle-group-inner' value={null} onClick={() => firstClick === 0 ? setFirstClick(null) : secondClick === 0 ? setSecondClick(null): secondClick}></div>
-                        <div className='middle-group-inner' value={5} onClick={() => firstClick === 0 ? setFirstClick(5) : secondClick === 0 ? setSecondClick(5): secondClick}>5</div>
-                        <div className='middle-group-inner' value={null} onClick={() => firstClick === 0 ? setFirstClick(null) : secondClick === 0 ? setSecondClick(null): secondClick}></div>
-                        <div className='middle-group-inner' value={1} onClick={() => firstClick === 0 ? setFirstClick(1) : secondClick === 0 ? setSecondClick(1): secondClick}>1</div>
-                        <div className='middle-group-inner' value={14} onClick={() => firstClick === 0 ? setFirstClick(14) : secondClick === 0 ? setSecondClick(14): secondClick}>14</div>
-                        <div className='middle-group-inner' value={3} onClick={() => firstClick === 0 ? setFirstClick(3) : secondClick === 0 ? setSecondClick(3): secondClick}>3</div>
+                        <div className='middle-group-inner' value={7} onClick={() => firstClick === null ? setFirstClick(7) : secondClick === null ? setSecondClick(7): secondClick}>7</div>
+                        <div className='middle-group-inner' value={8} onClick={() => firstClick === null ? setFirstClick(8) : secondClick === null ? setSecondClick(8): secondClick}>8</div>
+                        <div className='middle-group-inner' value={9} onClick={() => firstClick === null ? setFirstClick(9) : secondClick === null ? setSecondClick(9): secondClick}>9</div>
+                        <div className='middle-group-inner' value={null} onClick={() => firstClick === null ? setFirstClick(0) : secondClick === null ? setSecondClick(0): secondClick}></div>
+                        <div className='middle-group-inner' value={5} onClick={() => firstClick === null ? setFirstClick(5) : secondClick === null ? setSecondClick(5): secondClick}>5</div>
+                        <div className='middle-group-inner' value={null} onClick={() => firstClick === null ? setFirstClick(0) : secondClick === null ? setSecondClick(0): secondClick}></div>
+                        <div className='middle-group-inner' value={1} onClick={() => firstClick === null ? setFirstClick(1) : secondClick === null ? setSecondClick(1): secondClick}>1</div>
+                        <div className='middle-group-inner' value={14} onClick={() => firstClick === null ? setFirstClick(14) : secondClick === null ? setSecondClick(14): secondClick}>14</div>
+                        <div className='middle-group-inner' value={3} onClick={() => firstClick === null ? setFirstClick(3) : secondClick === null ? setSecondClick(3): secondClick}>3</div>
                     </div>
 
-                    <div className='second-row-inner' value={13} onClick={() => firstClick === 0 ? setFirstClick(13) : secondClick === 0 ? setSecondClick(13): secondClick}>13</div>
+                    <div className='second-row-inner' value={13} onClick={() => firstClick === null ? setFirstClick(13) : secondClick === null ? setSecondClick(13): secondClick}>13</div>
                 </div>
 
                 <div className='third-row'>
-                        <div value={10} onClick={() => firstClick === 0 ? setFirstClick(10) : secondClick === 0 ? setSecondClick(10): secondClick}>10</div>
-                        <div value={15} onClick={() => firstClick === 0 ? setFirstClick(15) : secondClick === 0 ? setSecondClick(15): secondClick}>15</div>
+                        <div value={10} onClick={() => firstClick === null ? setFirstClick(10) : secondClick === null ? setSecondClick(10): secondClick}>10</div>
+                        <div value={15} onClick={() => firstClick === null ? setFirstClick(15) : secondClick === null ? setSecondClick(15): secondClick}>15</div>
                 </div>
-        <div>Firstclick {firstClick}</div>
-        <div>Secondclick {secondClick}</div>
+
             <button onClick={reset}>Undo Pitch</button>
-            <button>Next Pitch</button>
+            <button onClick={nextPitch}>Next Pitch</button>
             <button>End and Save Session</button>
+            <div>Requested Pitch: {firstClick}</div>
+        <div>Actual Pitch: {secondClick}</div>
         </div>
     )
 }
